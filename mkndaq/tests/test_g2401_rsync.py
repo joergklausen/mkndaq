7b9482c679a56d183f1a653a2118d6e3f2f8b6ef
joerg.klausen <joerg.klausen@meteoswiss.ch>
2023-07-24 09:21:53 +0000

    wip

--- mkndaq/tests/test_g2401_rsync.py
+++ mkndaq/tests/test_g2401_rsync.py
@@ -10,33 +10,33 @@ import os
 from mkndaq.inst.g2401 import G2401
 
 def main():
-        cfg = {'file': 'mkndaq.cfg', 'version': '1.0.0-20210802', 'home': 'c:/users/mkn', 'reporting_interval': 10,
-                'logs': '~/Documents/devt/logs',
-                'data': '~/Documents/devt/data',
-                'staging': {'path': 'C:/Users/mkn/Documents/devt/staging', 'zip': True},
-                'g2401': {'type': 'G2401', 'serial_number': 'CFKADS2320',
-                        'staging_zip': True,
-                        'socket': {'host': '192.168.4.102', 'port': 51020, 'timeout': 5, 'sleep': 0.5},
-                        'get_data': ['_Meas_GetBufferFirst', '_Instr_getStatus'],
-                        'netshare': 'X:/',
-                        'days_to_sync': 7,
-                        'data_storage_interval': 'hourly',
-                        'staging_interval': 60,
-                        'staging_minute': 10,
-                        'sampling_interval': 5, 'aggregation_period': 600, 'reporting_interval': 600}
-                }
+    cfg = {'file': 'mkndaq.cfg', 'version': '1.0.0-20210802', 'home': 'c:/users/mkn', 'reporting_interval': 10,
+            'logs': '~/Documents/devt/logs',
+            'data': '~/Documents/devt/data',
+            'staging': {'path': 'C:/Users/mkn/Documents/devt/staging', 'zip': True},
+            'g2401': {'type': 'G2401', 'serial_number': 'CFKADS2320',
+                    'staging_zip': True,
+                    'socket': {'host': '192.168.4.102', 'port': 51020, 'timeout': 5, 'sleep': 0.5},
+                    'get_data': ['_Meas_GetBufferFirst', '_Instr_getStatus'],
+                    'netshare': 'DataLog_User_Sync',
+                    'days_to_sync': 7,
+                    'data_storage_interval': 'hourly',
+                    'staging_interval': 60,
+                    'staging_minute': 10,
+                    'sampling_interval': 5, 'aggregation_period': 600, 'reporting_interval': 600}
+            }
 
-        g2401 = G2401('g2401', config=cfg)
+    g2401 = G2401('g2401', config=cfg)
 
-        # %%
-        print("_Meas_GetConc:", g2401.tcpip_comm('_Meas_GetConc'))
-        #    print("_Instr_GetStatus:", g2401.tcpip_comm('_Instr_GetStatus'))
-        #    print("_Valves_Seq_Readstate:", g2401.tcpip_comm('_Valves_Seq_Readstate'))
-        #    print("_Meas_GetBuffer:", g2401.tcpip_comm('_Meas_GetBuffer'))
+    # %%
+    print("_Meas_GetConc:", g2401.tcpip_comm('_Meas_GetConc'))
+    #    print("_Instr_GetStatus:", g2401.tcpip_comm('_Instr_GetStatus'))
+    #    print("_Valves_Seq_Readstate:", g2401.tcpip_comm('_Valves_Seq_Readstate'))
+    #    print("_Meas_GetBuffer:", g2401.tcpip_comm('_Meas_GetBuffer'))
 
-        # %%
-        tst = g2401.store_and_stage_new_files()
-        print('done')
+    # %%
+    tst = g2401.store_and_stage_new_files()
+    print('done')
 
 if "__name__" == "__main__":
         pass
