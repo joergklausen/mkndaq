22c11abe1e8470e4d32c17bd03a85c11fcba8e3a
jkl <joerg.klausen@meteoswiss.ch>
2024-05-21 18:16:06 +0000

    wip neph integration

--- mkndaq/mkndaq.py
+++ mkndaq/mkndaq.py
@@ -43,11 +43,12 @@ def main():
     parser = argparse.ArgumentParser(
         description='Data acquisition and transfer for MKN Global GAW Station.',
         usage='mkndaq[.exe] [-s] -c')
-    parser.add_argument('-c', '--configuration', type=str, help='path to configuration file', required=True)
+    parser.add_argument('-c', '--configuration', type=str, help='path to configuration file', 
+                        default=os.path.expanduser('~dist/mkndaq.cfg'), required=False)
     parser.add_argument('-f', '--fetch', type=int, default=20,
                         help='interval in seconds to fetch and display current instrument data',
                         required=False)
-    parser.add_argument('-s', '--simulate', action='store_true',
+    parser.add_argument('-s', '--simulate', action='store_true', default=False,
                         help='simulate communication with instruments', required=False)
     args = parser.parse_args()
     simulate = args.simulate
@@ -75,12 +76,13 @@ def main():
 
     try:
         # initialize data transfer, set up remote folders
-        sftp = SFTPClient(config=cfg)
-        sftp.setup_remote_folders()
+        if cfg.get(['sftp'], None):
+            sftp = SFTPClient(config=cfg)
+            sftp.setup_remote_folders()
 
-        # stage most recent config file
-        print("%s Staging current config file ..." % time.strftime('%Y-%m-%d %H:%M:%S'))
-        sftp.stage_current_config_file(config_file)
+            # stage most recent config file
+            print("%s Staging current config file ..." % time.strftime('%Y-%m-%d %H:%M:%S'))
+            sftp.stage_current_config_file(config_file)
 
         # initialize instruments, get and set configurations and define schedules
         # NB: In case more instruments should be handled, the relevant calls need to be included here below.
@@ -88,50 +90,51 @@ def main():
             if cfg.get('tei49c', None):
                 from mkndaq.inst.tei49c import TEI49C
                 tei49c = TEI49C(name='tei49c', config=cfg, simulate=simulate)
-                schedule.every(cfg['tei49c']['sampling_interval']).minutes.at(':00').do(tei49c.get_data)
-                schedule.every(6).hours.at(':00').do(tei49c.set_datetime)
-                schedule.every(fetch).seconds.do(tei49c.print_o3)
+                schedule.every(cfg['tei49c']['sampling_interval']).minutes.at(':00').do(run_threaded, tei49c.get_data)
+                schedule.every(6).hours.at(':00').do(run_threaded, tei49c.set_datetime)
+                schedule.every(fetch).seconds.do(run_threaded, tei49c.print_o3)
             if cfg.get('tei49i', None):
                 from mkndaq.inst.tei49i import TEI49I
                 tei49i = TEI49I(name='tei49i', config=cfg, simulate=simulate)
-                schedule.every(cfg['tei49i']['sampling_interval']).minutes.at(':00').do(tei49i.get_data)
-                schedule.every().day.at('00:00').do(tei49i.set_datetime)
-                schedule.every(fetch).seconds.do(tei49i.print_o3)
+                schedule.every(cfg['tei49i']['sampling_interval']).minutes.at(':00').do(run_threaded, tei49i.get_data)
+                schedule.every().day.at('00:00').do(run_threaded, tei49i.set_datetime)
+                schedule.every(fetch).seconds.do(run_threaded, tei49i.print_o3)
             if cfg.get('tei49i_2', None):
                 from mkndaq.inst.tei49i import TEI49I
                 tei49i_2 = TEI49I(name='tei49i_2', config=cfg, simulate=simulate)
-                schedule.every(cfg['tei49i_2']['sampling_interval']).minutes.at(':00').do(tei49i_2.get_data)
-                schedule.every().day.at('00:00').do(tei49i_2.set_datetime)
-                schedule.every(fetch+5).seconds.do(tei49i_2.print_o3)
+                schedule.every(cfg['tei49i_2']['sampling_interval']).minutes.at(':00').do(run_threaded, tei49i_2.get_data)
+                schedule.every().day.at('00:00').do(run_threaded, tei49i_2.set_datetime)
+                schedule.every(fetch+5).seconds.do(run_threaded, tei49i_2.print_o3)
             if cfg.get('g2401', None):
                 from mkndaq.inst.g2401 import G2401
                 g2401 = G2401('g2401', config=cfg)
                 g2401.store_and_stage_files()
-                schedule.every(cfg['g2401']['staging_interval']).minutes.do(g2401.store_and_stage_files)
-                schedule.every(fetch).seconds.do(g2401.print_co2_ch4_co)
+                schedule.every(cfg['g2401']['staging_interval']).minutes.do(run_threaded, g2401.store_and_stage_files)
+                schedule.every(fetch).seconds.do(run_threaded, g2401.print_co2_ch4_co)
             if cfg.get('meteo', None):
                 from mkndaq.inst.meteo import METEO
                 meteo = METEO('meteo', config=cfg)
                 meteo.store_and_stage_files()
-                schedule.every(cfg['meteo']['staging_interval']).minutes.do(meteo.store_and_stage_files)
-                schedule.every(cfg['meteo']['staging_interval']).minutes.do(meteo.print_meteo)
+                schedule.every(cfg['meteo']['staging_interval']).minutes.do(run_threaded, meteo.store_and_stage_files)
+                schedule.every(cfg['meteo']['staging_interval']).minutes.do(run_threaded, meteo.print_meteo)
             if cfg.get('aerosol', None):
                 from mkndaq.inst.aerosol import AEROSOL
                 aerosol = AEROSOL('aerosol', config=cfg)
                 aerosol.store_and_stage_files()
-                schedule.every(cfg['aerosol']['staging_interval']).minutes.do(aerosol.store_and_stage_files)
-                schedule.every(cfg['aerosol']['staging_interval']).minutes.do(aerosol.print_aerosol)
+                schedule.every(cfg['aerosol']['staging_interval']).minutes.do(run_threaded, aerosol.store_and_stage_files)
+                schedule.every(cfg['aerosol']['staging_interval']).minutes.do(run_threaded, aerosol.print_aerosol)
             if cfg.get('ae33', None):
                 from mkndaq.inst.ae33 import AE33
                 ae33 = AE33(name='ae33', config=cfg)
                 schedule.every(cfg['ae33']['sampling_interval']).minutes.at(':00').do(ae33.get_new_data)
                 schedule.every(cfg['ae33']['sampling_interval']).minutes.at(':00').do(ae33.get_new_log_entries)
-                schedule.every(fetch).seconds.do(ae33.print_ae33)
+                schedule.every(fetch).seconds.do(run_threaded, ae33.print_ae33)
             if cfg.get('ne300', None):
                 from mkndaq.inst.neph import NEPH
                 ne300 = NEPH(name='ne300', config=cfg)
-                schedule.every(cfg['ne300']['sampling_interval']).minutes.at(':00').do(ne300.get_new_data)
-                schedule.every(fetch).seconds.do(ne300.print_ssp_bssp)
+                schedule.every(fetch).seconds.do(run_threaded, ne300.print_ssp_bssp)
+                schedule.every(cfg['ne300']['sampling_interval']).minutes.at(':00').do(run_threaded, ne300.get_new_data)
+                schedule.every(cfg['ne300']['zero_span_check_interval']).minutes.at(':00').do(run_threaded, ne300.do_zero_span_check)
 
         except Exception as err:
             if logs:
