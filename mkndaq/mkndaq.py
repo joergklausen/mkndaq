ec1ac45c0071ebbc123ffb31d6c4433ce16d6f20
joerg.klausen <joerg.klausen@meteoswiss.ch>
2024-11-08 16:20:31 +0000

    fix(mkndaq.py):resolution of count down timer

--- mkndaq/mkndaq.py
+++ mkndaq/mkndaq.py
@@ -181,7 +181,7 @@ def main():
         # align start with a multiple-of-minute timestamp
         seconds_left = seconds_to_next_n_minutes(1)
         while seconds_left > 0:
-            print(f"Time remaining: {seconds_left} seconds", end="\r")
+            print(f"Time remaining: {seconds_left:0.1f} s", end="\r")
             dt = 0.2
             time.sleep(dt)
             seconds_left -= dt
