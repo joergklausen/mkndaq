e39a6f96d1a7e95e87324f6d69f05f1173855ca8
jkl <joerg.klausen@meteoswiss.ch>
2026-01-20 07:50:39 +0000

    fix(mkndaq-fallback.bat):update call to main script

--- mkndaq-fallback.bat
+++ mkndaq-fallback.bat
@@ -4,7 +4,7 @@ set git=%root%/git/mkndaq
 set venv=%git%/.venv/Scripts
 rem set config=%git%/dist/mkndaq-fallback.yml
 set log=%root%/mkndaq/mkndaq-start.log
-set script=%git%/__main-fallback__.py
+set script=%git%/__main__.py -c %git%/dist/mkndaq-fallback.yml
 
 rem record execution of mkndaq.bat to logfile.
 echo %date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,8% mkndaq-fallback.bat started. >> %log%
