fe5b5e1573f3e49ca161cd0f02c71d4ee6c9fe9f
Jörg Klausen <joerg.klausen@meteoswiss.ch>
2026-03-15 10:07:25 +0000

    fix(mkndaq.yml,mkndaq.py):print to console interval;re-include all instrs

--- mkndaq/mkndaq.py
+++ mkndaq/mkndaq.py
@@ -57,7 +57,7 @@ def main():
         description='Data acquisition and transfer for MKN Global GAW Station.',
         usage='python3 mkndaq.py|mkndaq.exe -c mkndaq.yml',
     )
-    parser.add_argument('-c', '--config', default='mkndaq.yml', help='path to yml config file')
+    parser.add_argument('-c', '--config', default='./dist/mkndaq.yml', help='path to yml config file')
     args = parser.parse_args()
 
     # read config file and set up logging
@@ -84,7 +84,7 @@ def main():
         logger.info(f"[mkndaq] started, version={version}, config={cfgfile}")
 
         # global settings
-        fetch = cfg.get('fetch_interval_seconds', 5)
+        fetch = cfg.get('print_interval_seconds', 20)
 
         # configure transfer clients (S3, SFTP)
         s3fsc = None
@@ -126,6 +126,132 @@ def main():
 
                 schedule.every(fetch).seconds.do(run_threaded, tei49c.print_o3)
                 logger.info(f"[tei49c] setup complete")
+
+            if cfg.get('tei49i', None):
+                from mkndaq.inst.thermo import Thermo49i
+                tei49i = Thermo49i(name='tei49i', config=cfg)
+                tei49i.setup_schedules()
+
+                schedule.every(fetch).seconds.do(run_threaded, tei49i.print_o3)
+                schedule.every().day.at('00:05').do(run_threaded, tei49i.set_datetime)
+
+                if s3fsc:
+                    s3fsc.setup_transfer_schedules(
+                        local_path=str(tei49i.staging_path),
+                        key_prefix=tei49i.remote_path,
+                        interval=tei49i.reporting_interval,
+                        delay_transfer=2,
+                        remove_on_success=False,
+                    )
+                if sftp:
+                    remote_path = (PurePosixPath(sftp.remote_path) / tei49i.remote_path).as_posix()
+                    sftp.setup_transfer_schedules(local_path=str(tei49i.staging_path),
+                                                  remote_path=remote_path,
+                                                  interval=tei49i.reporting_interval,
+                                                  delay_transfer=15,
+                                                  remove_on_success=True,
+                                                  )
+                logger.info(f"[tei49i] setup complete")
+
+            if cfg.get('tei49i_2', None):
+                from mkndaq.inst.thermo import Thermo49i
+                tei49i_2 = Thermo49i(name='tei49i_2', config=cfg)
+                tei49i_2.setup_schedules()
+
+                if s3fsc:
+                    s3fsc.setup_transfer_schedules(
+                        local_path=str(tei49i_2.staging_path),
+                        key_prefix=tei49i_2.remote_path,
+                        interval=tei49i_2.reporting_interval,
+                        remove_on_success=False,
+                    )
+                if sftp:
+                    remote_path = (PurePosixPath(sftp.remote_path) / tei49i_2.remote_path).as_posix()
+                    sftp.setup_transfer_schedules(local_path=str(tei49i_2.staging_path),
+                                                  remote_path=remote_path,
+                                                  interval=tei49i_2.reporting_interval)
+
+                schedule.every().day.at('00:00').do(run_threaded, tei49i_2.set_datetime)
+                schedule.every(fetch+5).seconds.do(run_threaded, tei49i_2.print_o3)
+
+            if cfg.get('tei49c-ps', None):
+                from mkndaq.inst.thermo import Thermo49CPS
+                tei49cps = Thermo49CPS(name='tei49c-ps', config=cfg)
+                tei49cps.get_config()
+                tei49cps.set_config()
+                tei49cps.setup_schedules()
+
+                schedule.every(fetch).seconds.do(run_threaded, tei49cps.print_o3)
+                schedule.every().day.at('00:03').do(run_threaded, tei49c.set_datetime)
+
+                if s3fsc:
+                    s3fsc.setup_transfer_schedules(
+                        local_path=str(tei49cps.staging_path),
+                        key_prefix=tei49cps.remote_path,
+                        interval=tei49cps.reporting_interval,
+                        delay_transfer=3,
+                        remove_on_success=False,
+                    )
+                if sftp:
+                    remote_path = (PurePosixPath(sftp.remote_path) / tei49cps.remote_path).as_posix()
+                    sftp.setup_transfer_schedules(local_path=str(tei49cps.staging_path),
+                                                  remote_path=remote_path,
+                                                  interval=tei49cps.reporting_interval, 
+                                                  delay_transfer=15,
+                                                  remove_on_success=True,
+                                                  )
+
+            if cfg.get('g2401', None):
+                from mkndaq.inst.g2401 import G2401
+                g2401 = G2401('g2401', config=cfg)
+                g2401.store_and_stage_files()
+
+                schedule.every(fetch).seconds.do(run_threaded, g2401.print_co2_ch4_co)
+                schedule.every(cfg['g2401']['reporting_interval']).minutes.do(run_threaded, g2401.store_and_stage_files)
+
+                if s3fsc:
+                    s3fsc.setup_transfer_schedules(
+                        local_path=str(g2401.staging_path),
+                        key_prefix=g2401.remote_path,
+                        interval=g2401.reporting_interval,
+                        delay_transfer=2,
+                        remove_on_success=False,
+                    )
+                if sftp:
+                    remote_path = (PurePosixPath(sftp.remote_path) / g2401.remote_path).as_posix()
+                    sftp.setup_transfer_schedules(local_path=str(g2401.staging_path),
+                                                  remote_path=remote_path,
+                                                  interval=g2401.reporting_interval,
+                                                  delay_transfer=15,
+                                                  remove_on_success=True,
+                                                  )
+                logger.info(f"[g2401] setup complete")
+
+            if cfg.get('meteo', None):
+                from mkndaq.inst.meteo import METEO
+                meteo = METEO('meteo', config=cfg)
+                meteo.store_and_stage_files()
+
+                schedule.every(cfg['meteo']['reporting_interval']).minutes.do(run_threaded, meteo.print_meteo)
+                schedule.every(cfg['meteo']['reporting_interval']).minutes.do(run_threaded, meteo.store_and_stage_files)
+
+                if s3fsc:
+                    s3fsc.setup_transfer_schedules(
+                        local_path=str(meteo.staging_path),
+                        key_prefix=meteo.remote_path,
+                        interval=meteo.reporting_interval,
+                        delay_transfer=2,
+                        remove_on_success=False,
+                    )
+                if sftp:
+                    remote_path = (PurePosixPath(sftp.remote_path) / meteo.remote_path).as_posix()
+                    sftp.setup_transfer_schedules(local_path=str(meteo.staging_path),
+                                                  remote_path=remote_path,
+                                                  interval=meteo.reporting_interval,
+                                                  delay_transfer=15,
+                                                  remove_on_success=True,
+                                                  )
+                logger.info(f"[meteo] setup complete")
 
             if cfg.get('ne300', None):
                 from mkndaq.inst.neph import NEPH
